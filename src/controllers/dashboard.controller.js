9385bf5e798159381004018f6bdf0c222b945949
Aman  Waghmare <93338002+Amu757@users.noreply.github.com>
2024-06-07 23:05:00 +0530

    Update dashboard.controller.js

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,17 +7,42 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 import {Video} from "../model/video.model.js"
+import {Like} from "../model/like.model.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+//total views
+//total subscribers
+//total likes
+  
+//total videos
+  const allVideos = await Video.aggregate ([
+        {
+            $match:{  // i think this will give me all the matching videos having same _id
+                owner._id:user._id
+            }
+        },
+    ])
+
+const totalVideos = allVideos.length()
+    
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
     const user = req.user;
 
-    //const allVideos = Video.findById(user._id)  //little confused how to search the owner by using user._id
+    const allVideos = await Video.aggregate ([
+        {
+            $match:{  // i think this will give me all the matching videos having same _id
+                owner._id:user._id
+            }
+        },
+    ])
 
+    return res.status(200).json(
+        new apiResponse(200,allVideos,"all videos of the current channel are fetched successfuly."
+    )
     
 })
 
